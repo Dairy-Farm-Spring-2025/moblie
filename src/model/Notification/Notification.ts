610ade4e36269ff714158f5e392fd6ab4ff428df
namhuu6969 <namquangbinh6969@gmail.com>
2025-03-29 16:32:07 +0700

    Notification screen

---
+++ src/model/Notification/Notification.ts
@@ -0,0 +1,23 @@
+import { User } from '@model/User/User';
+
+export type Notification = {
+  id: {
+    userId: number;
+    notificationId: number;
+  };
+  user: User;
+  notification: {
+    notificationId: number;
+    title: string;
+    description: string;
+    link: string;
+    category: CategoryNotification;
+  };
+  read: boolean;
+};
+export type CategoryNotification =
+  | 'milking'
+  | 'feeding'
+  | 'heathcare'
+  | 'task'
+  | 'other';
---
+++ src/screens/NotificationScreen/components/CardNotification.tsx
@@ -0,0 +1,103 @@
+import { COLORS } from '@common/GlobalStyle';
+import CardComponent from '@components/Card/CardComponent';
+import { Notification } from '@model/Notification/Notification';
+import { formatCamelCase } from '@utils/format';
+import { t } from 'i18next';
+import React from 'react';
+import { Alert, TouchableOpacity, View } from 'react-native';
+import { Text } from 'react-native-paper';
+import { Ionicons } from '@expo/vector-icons';
+import { useMutation, useQueryClient } from 'react-query';
+import apiClient from '@config/axios/axios';
+
+interface CardNotificationProps {
+  item: Notification;
+}
+
+const getCategoryIcon = (category: string) => {
+  switch (category) {
+    case 'milking':
+      return 'beaker';
+    case 'feeding':
+      return 'fast-food-outline';
+    case 'heathcare':
+      return 'medkit-outline';
+    case 'task':
+      return 'clipboard-outline';
+    default:
+      return 'notifications-outline';
+  }
+};
+
+const CardNotification = ({ item }: CardNotificationProps) => {
+  const queryClient = useQueryClient();
+
+  const { mutate } = useMutation(
+    async ({
+      userId,
+      notificationId,
+    }: {
+      userId: number;
+      notificationId: number;
+    }) =>
+      await apiClient.put(
+        `notifications/${notificationId}/mark-read/${userId}`
+      ),
+    {
+      onSuccess: () => {
+        console.log('Success');
+        queryClient.invalidateQueries('notifications/myNotification');
+      },
+      onError: (error: any) => {
+        Alert.alert('Error', error.response.data.message);
+      },
+    }
+  );
+  const onClickRead = (userId: number, notificationId: number) => {
+    if (!item.read) {
+      mutate({ userId, notificationId });
+    }
+  };
+  return (
+    <TouchableOpacity
+      onPress={() => onClickRead(item.id.userId, item.id.notificationId)}
+      style={{ marginBottom: 15 }}
+    >
+      <CardComponent
+        style={{
+          padding: 15,
+          backgroundColor:
+            item?.read === true ? '#FFF' : 'rgba(45, 249, 133, 0.25)',
+        }}
+      >
+        <View style={{ flexDirection: 'column', gap: 15 }}>
+          <View>
+            <Text style={{ fontSize: 20, fontWeight: '600' }}>
+              {item.notification.title}
+            </Text>
+            <View
+              style={{
+                flexDirection: 'row',
+                gap: 2,
+                alignItems: 'center',
+                marginTop: 5,
+              }}
+            >
+              <Ionicons
+                name={getCategoryIcon(item.notification.category)}
+                size={13}
+                color={COLORS.primary}
+              />
+              <Text style={{ fontSize: 13, fontWeight: '600' }}>
+                {t(formatCamelCase(item.notification.category))}
+              </Text>
+            </View>
+          </View>
+          <Text style={{ fontSize: 15 }}>{item.notification.description}</Text>
+        </View>
+      </CardComponent>
+    </TouchableOpacity>
+  );
+};
+
+export default CardNotification;
